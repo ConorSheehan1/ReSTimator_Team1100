1c005b35574550205b5dff25498379ab652d2cc3
8ballbb <andrewpoolejames@gmail.com>
2016-06-30 19:18:26 +0100

    some initial styling with bootstrap

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import restimatorApp
 from .login import LoginForm
 
--- untitled.py
+++ untitled.py
@@ -85,3 +85,56 @@
 # 	<script type="text/javascript" src="/static/scripts/main.js"></script>
 # </body>
 # </html>
+
+# <!DOCTYPE html>
+# <html lang="en">
+# <head>
+# 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+# 	<meta charset="UTF-8">
+# 	<!-- SUPPORT FOR IE6-8 OF HTML5 ELEMENTS -->
+# 	<!--[if lt IE 9]>
+# 	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+# 	<![endif]-->
+
+# 	<title>ReSTimator - {{ username }} </title>
+
+# 	<!-- style sheets -->
+# 	<!-- <link rel="stylesheet" type="text/css" href="/static/css/reset.css"> -->
+# 	<!-- <link rel="stylesheet" type="text/css" href="/static/css/style.css"> -->
+# 	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	
+# 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+# 	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+# </head>
+# <body>
+# 	<!-- <div id="page" data-role="page"> -->
+# 	<div class="container">
+		
+# 		<header>
+
+# 			<h1>ReSTimator Image Goes Here<h1>
+
+# 		</header>
+		
+# 		<div id="navigation">
+
+# 			<ul>
+# 				<li><a href="#">Login</a></li>
+# 				<li><a href="#">Upload</a></li>
+# 				<li><a href="#">Application</a></li>
+# 			</ul>
+
+# 		</div>
+
+# 		<div id="content">
+# 			{% block content %}{% endblock %} <!-- Block control statement: where other templates can insert themselves -->
+# 		</div>
+		
+# 		<footer>
+		
+# 		</footer>
+# 	</div>
+# 	<!-- Main script -->
+# 	<script type="text/javascript" src="/static/scripts/main.js"></script>
+# </body>
+# </html>
