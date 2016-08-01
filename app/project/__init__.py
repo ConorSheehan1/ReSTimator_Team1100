50877e1f52af4e7503c2ffe68ca7fbb8761ff5d4
stephenmurphy <stephen.murphy.6@ucdconnect.ie>
2016-08-01 20:36:14 +0100

    Added basic upload functionality

--- app/project/__init__.py
+++ app/project/__init__.py
@@ -10,6 +10,8 @@ lm = LoginManager()
 lm.init_app(restimatorApp)
 restimatorApp.config.from_object("config.DevelopmentConfig") # read and use config file
 db = SQLAlchemy(restimatorApp) # sqlalchemy database object
+restimatorApp.config['UPLOAD_FOLDER'] = './data/log_data' # This is the path to the upload directory
+restimatorApp.config['ALLOWED_EXTENSIONS'] = set(['txt', 'xlsx', 'csv']) # These are the extension that we are accepting to be uploaded
 
 from project.users.views import users_blueprint
 from project.analysis.views import analysis_blueprint
