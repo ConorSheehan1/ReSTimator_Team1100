2da303a0d4e13737123d3b875d5e5c7dc16502ae
Conor Sheehan <conor.sheehan.2@ucdconnect.ie>
2016-08-03 11:49:35 +0100

    get all values from row

--- app/import_csv.py
+++ app/import_csv.py
@@ -1,4 +1,7 @@
-import sqlite3
+'''
+http://stackoverflow.com/questions/16947276/flask-sqlalchemy-iterate-column-values-on-a-single-row
+'''
+
 from data.extract_log_data import *
 from project import db # import database object
 from project.models import *
@@ -9,11 +12,11 @@ if __name__ == "__main__":
 
     # conn = sqlite3.connect("./project/test.db")
     # cur = conn.cursor()
-    logs = log_df("./data/temp_csv/")
+    df = log_df("./data/temp_csv/")
     # print(logs)
 
     # insert dataframe into db line by line
-    for i, row in logs.iterrows():
+    for i, row in df.iterrows():
 
         logs = Occupy(room=row["room"], date=row["date"], time=row["time"],
                       associated_client_count=row["associated_client_count"],
@@ -34,7 +37,15 @@ if __name__ == "__main__":
             # df_copy = row.copy(deep=True)
             # print(df_copy)
 
-            print(db_row.date)
+            # df_db = pd.DataFrame(columns=logs.columns, data=db_row.values)
+
+            # print(db_row.time)
+            # values = [val for val in dir(db_row) if val in df.columns]
+            # print(values)
+
+            db_row_dict = dict((col, getattr(db_row, col)) for col in db_row.__table__.columns.keys())
+            print(db_row_dict)
+
             continue
 
 
