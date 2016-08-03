6c816eef30353d1dff4a4ebcfd3f5084b5d3b5b6
Conor Sheehan <conor.sheehan.2@ucdconnect.ie>
2016-08-03 11:31:26 +0100

    get rows which cause integrity error when exception is thrown

--- app/import_csv.py
+++ app/import_csv.py
@@ -1,8 +1,9 @@
 import sqlite3
 from data.extract_log_data import *
-# from project import db # import database object
+from project import db # import database object
 from project.models import *
 import sqlalchemy
+import pandas as pd
 
 if __name__ == "__main__":
 
@@ -11,19 +12,31 @@ if __name__ == "__main__":
     logs = log_df("./data/temp_csv/")
     # print(logs)
 
-    for tup in logs.itertuples():
-        logs = Occupy(room=tup[1], date=int(tup[2]), time=tup[3], associated_client_count=int(tup[4]),
-                      authenticated_client_count=int(tup[5]), module_code=None, occupancy=None)
+    # insert dataframe into db line by line
+    for i, row in logs.iterrows():
+
+        logs = Occupy(room=row["room"], date=row["date"], time=row["time"],
+                      associated_client_count=row["associated_client_count"],
+                      authenticated_client_count=row["authenticated_client_count"],
+                      module_code=None, occupancy=None)
         db.session.add(logs)
+
+        # if commiting to db fails, get row with integrity constraint and merge, then try to commit
         try:
             db.session.commit()
 
         # except sqlite3.IntegrityError:
         except sqlalchemy.exc.IntegrityError:
-            print("integrity error")
-            # logs = Occupy(room=tup[1], date=tup[2], time=tup[3], associated_client_count=tup[4],
-            #               authenticated_client_count=tup[5], module_code=None, occupancy=None)
-            # db.session.update(logs)
+            db.session.rollback()
+            print("integrity error", row["time"])
+            db_row = Occupy.query.filter_by(date=row["date"], time=row["time"], room=row["room"]).first()
+
+            # df_copy = row.copy(deep=True)
+            # print(df_copy)
+
+            print(db_row.date)
+            continue
+
 
 
 
@@ -35,9 +48,17 @@ if __name__ == "__main__":
 
 
 
+'''
+    for tup in logs.itertuples():
+        logs = Occupy(room=tup[1], date=int(tup[2]), time=tup[3], associated_client_count=int(tup[4]),
+                      authenticated_client_count=int(tup[5]), module_code=None, occupancy=None)
+        db.session.add(logs)
 
+        # if commiting to db fails, get row with integrity constraint and merge, then try to commit
+        try:
+            db.session.commit()
 
-# # sqlalchemy
-# print(db)
-#
-# # don't know how to tup df into db efficiently (have to use name of every column?
+        # except sqlite3.IntegrityError:
+        except sqlalchemy.exc.IntegrityError:
+            print("integrity error")
+                '''
