7ab77b370c2abff6a7f89e67ee3499823cf0de0c
Conor Sheehan <conor.sheehan.2@ucdconnect.ie>
2016-08-03 12:51:34 +0100

    covert update db into function

--- app/import_csv.py
+++ app/import_csv.py
@@ -1,20 +1,15 @@
 '''
 http://stackoverflow.com/questions/16947276/flask-sqlalchemy-iterate-column-values-on-a-single-row
+http://stackoverflow.com/questions/270879/efficiently-updating-database-using-sqlalchemy-orm
 '''
 
 from data.extract_log_data import *
 from project import db # import database object
 from project.models import *
 import sqlalchemy
-import pandas as pd
 
-if __name__ == "__main__":
 
-    # conn = sqlite3.connect("./project/test.db")
-    # cur = conn.cursor()
-    df = log_df("./data/temp_csv/")
-    # print(logs)
-
+def update_db(df):
     # insert dataframe into db line by line
     for i, row in df.iterrows():
 
@@ -35,43 +30,29 @@ if __name__ == "__main__":
             print("integrity error @", row["room"], row["date"], row["time"])
             print("trying to update db ...")
             # get row which causes integrity constraint
-            db_row = Occupy.query.filter_by(date=row["date"], time=row["time"], room=row["room"]).first()
+            # db_row = Occupy.query.filter_by(date=row["date"], time=row["time"], room=row["room"]).first()
+            db_row = db.session.query(Occupy).filter_by(date=row["date"], time=row["time"], room=row["room"]).first()
 
             # convert the sqlalchemy object and pandas row to dictionaries so they can be merged
             dict_db_row = dict((column, getattr(db_row, column)) for column in db_row.__table__.columns.keys())
             dict_row = dict((column, row[column]) for column in df.columns)
 
-            # test that rows are updated correctly
-            dict_row["room"] = "B5000"
-            print(dict_db_row, "\n", dict_row)
-
             # overwrite values in dict_row with values already in dict_db_row
             # in other words, only add values from row that are not already in the db
             merged = dict(list(dict_row.items()) + list(dict_db_row.items()))
             print(merged)
+            merged["module_code"] = "asdf"
 
-            updated = logs(room=merged["room"], date=merged["date"], time=merged["time"],
-                           associated_client_count=merged["associated_client_count"],
-                           authenticated_client_count=merged["authenticated_client_count"],
-                           module_code=merged["module_code"], occupancy=merged["occupancy"])
-
-            db.session.add(updated)
+            db_row.module_code = merged["module_code"]
 
             try:
                 db.session.commit()
+                print("db updated")
             except sqlalchemy.exc.IntegrityError:
                 print("data already in db", dict_row)
                 db.session.rollback()
                 continue
-
             continue
 
-
-            # df_copy = row.copy(deep=True)
-            # print(df_copy)
-
-            # df_db = pd.DataFrame(columns=logs.columns, data=db_row.values)
-
-            # print(db_row.time)
-            # values = [val for val in dir(db_row) if val in df.columns]
-            # print(values)
+if __name__ == "__main__":
+    update_db(log_df("./data/temp_csv/"))
