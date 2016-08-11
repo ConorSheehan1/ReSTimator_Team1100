2f0a4bccdfc71d7ebe9b08f183196fbc2b28b9bc
8ballbb <andrewpoolejames@gmail.com>
2016-08-11 13:49:12 +0100

    Heroku v1

--- app/run.py
+++ app/run.py
@@ -1,4 +1,6 @@
 from project import restimatorApp
+import os
 
 if __name__ == "__main__": 
-    restimatorApp.run() # Start server with run method
+    port = int(os.environ.get("PORT", 5000))
+    restimatorApp.run(host='0.0.0.0', port=port) # Start server with run method
